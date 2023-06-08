a82131b1eb15f436535cd6216bf10f9a5d6aeaac
Ramón Canela Villamizar <rcanelav@gmail.com>
2023-06-08 15:00:51 +0200

    UPDATE: select env

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
-require('dotenv').config();
+require('dotenv').config(
+    {
+        path: `.env.${process.env.NODE_ENV}`
+    }
+);
 const Server = require('./app/models/Server');
 
 const server = new Server();
